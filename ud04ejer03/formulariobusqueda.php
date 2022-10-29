0577ac95bc0cbe8233377aafe52a533dfc5c4b97
mohasb <mhabnd@gmail.com>
2022-10-29 17:06:35 +0200

    Initial commit

---
+++ ud04ejer03/formulariobusqueda.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Paises</title>
+</head>
+
+<body>
+    <form action="resultadopaises.php" method="get">
+        <label>Escribe el pais: <input type="text" name="pais" /></label>
+        <input type="submit" name="enviando" value="BUSCAR" />
+    </form>
+</body>
+
+</html>
---
+++ ud04ejer03/formulariointroduccion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario Introducción</title>
+</head>
+
+<body style="background-color: darkgray;">
+<h1>Dar de alta ARTÍCULOS NUEVOS</h1>
+    <form action="" method="">
+    <label for="seccion" style="display: inline-block;width: 150px;">Sección del articulo</label>
+    <input type="text" name="seccion">
+    <br>
+    <label for="nombre" style="display: inline-block;width: 150px;">Nombre del articulo</label>
+    <input type="text" name="nombre">
+    <br>
+    <label for="precio" style="display: inline-block;width: 150px;">Precio del articulo</label>
+    <input type="text">
+    <br>
+    <label for="pais" style="display: inline-block;width: 150px;">Pais origen articulo</label>
+    <input type="text" name="pais">
+    <br>
+    <br>
+    <input type="submit" value="Insertar!!">
+    </form>
+</body>
+
+</html>
